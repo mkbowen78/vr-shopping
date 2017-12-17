11176a841cf710ec8996016485539d2002bc1c41
mkbowen78 <mkbowen78@gmail.com>
2017-12-16 23:28:19 -0600

    working basic VR scene with React

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,8 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'aframe';
+import {Entity, Scene} from 'aframe-react';
+import 'babel-polyfill';
+import 'aframe-particle-system-component';
+
 class App extends React.Component {
   render () {
-    return (<div>hello world</div>)
+    return (
+      <Scene>
+        <Entity geometry={{primitive: 'box'}} material={{color: 'red'}} position={{x: 0, y: 0, z: -5}}/>
+        <Entity particle-system={{preset: 'snow'}}/>
+        <Entity light={{type: 'point'}}/>
+        <Entity gltf-model={{src: 'virtualcity.gltf'}}/>
+        <Entity text={{value: 'Welcome to Kevin's VR World!'}}/>
+      </Scene>
+    )
   }
 }
 ReactDOM.render(<App />, document.getElementById('app'));
--- server/index.js
+++ server/index.js
@@ -2,5 +2,5 @@ const express = require('express');
 var app = express();
 app.use(express.static(__dirname + '/../client/dist'));
 app.listen(5000, () => {
-  console.log(`listening on port 5000!`);
+  console.log(`It works! Listening on port 5000!`);
 });
